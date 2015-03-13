e06aaa7345ea9395e6a9666502a60c52e511014e
Ken Perkins <ken.perkins@rackspace.com>
2015-03-13 08:15:02 -0700

    Adding a basic logging setup with Winston
    - Fixes #3

--- app.js
+++ app.js
@@ -6,11 +6,16 @@
  */
 
 var async = require('async'),
+  logging = require('./src/logging'),
   pkgcloud = require('pkgcloud'),
   restify = require('restify'),
-  version = require('./package.json').version;
+  routes = require('./src/routes'),
+  info = require('./package.json');
 
-var server = restify.createServer();
+var server = restify.createServer(),
+  log = logging.getLogger(process.env.CONTENT_LOG_LEVEL || 'info');
+
+server.name = info.name;
 
 // TODO fully rationalize a strategy for ensuring configuration is correct
 
@@ -31,83 +36,15 @@ var client = pkgcloud.providers.rackspace.storage.createClient({
 // Setup some ghetto middleware
 server
   .use(function foo(req, res, next) {
-    console.log(req.method + ' ' + req.url);
+    log.verbose(req.method + ' ' + req.url);
     next();
   })
   .use(restify.fullResponse())
   .use(restify.bodyParser());
 
-/**
- * @description gets the version of the current service
- */
-server.get('/version', function(req, res, next) {
-  res.send(version);
-  next();
-});
-
-/**
- * @description Allows retrieving content from the content service
- */
-server.get('/content/:id', function(req, res, next) {
-  var source = client.download({
-    container: process.env.RACKSPACE_CONTAINER,
-    remote: encodeURIComponent(req.params.id)
-  });
-
-  // This directly sends the response to the caller, long term we'll
-  // probably not want to do this, but it allows the prototype to get functional
-  //
-  source.pipe(res);
-
-  next();
-});
-
-/**
- * @description Allows storing new content into the content service
- *
- * Payload must be in the form of:
- *
- * {
- *   id: "https://github.com/deconst/deconst-docs/issues/16" // Full url of content
- *   body: { }
- * }
- *
- */
-server.put('/content', function(req, res, next) {
-  var dest = client.upload({
-    container: process.env.RACKSPACE_CONTAINER,
-    remote: encodeURIComponent(req.body.id)
-  });
-
-  dest.on('success', function() {
-    res.status(200);
-    res.send();
-    next();
-  });
-
-  // For now we're just going to JSON.stringify the body directly up to cloud files
-  // longer term we might do multi-plexing or async.parallel to different stores
-  dest.end(JSON.stringify(req.body.body));
-});
-
-/**
- * @description Delete a piece of content by id
- */
-server.del('/content/:id', function (req, res, next) {
-  client.removeFile(process.env.RACKSPACE_CONTAINER, encodeURIComponent(req.params.id), function(err) {
-    if (err) {
-      res.status(err.statusCode);
-      res.send();
-      next();
-      return;
-    }
-
-    res.status(200);
-    res.send();
-    next();
-  });
-});
+// this is kind of hacky, but for now it keeps our routes a bit less messy
+routes.loadRoutes(server, info);
 
 server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+  log.info('%s listening at %s', server.name, server.url);
 });
---
+++ src/routes.js
@@ -0,0 +1,76 @@
+
+
+exports.loadRoutes = function(server, info) {
+  /**
+   }
+   * @description gets the version of the current service
+   */
+  server.get('/version', function (req, res, next) {
+    res.send(info.version);
+    next();
+  });
+
+  /**
+   * @description Allows retrieving content from the content service
+   */
+  server.get('/content/:id', function (req, res, next) {
+    var source = client.download({
+      container: process.env.RACKSPACE_CONTAINER,
+      remote: encodeURIComponent(req.params.id)
+    });
+
+    // This directly sends the response to the caller, long term we'll
+    // probably not want to do this, but it allows the prototype to get functional
+    //
+    source.pipe(res);
+
+    next();
+  });
+
+  /**
+   * @description Allows storing new content into the content service
+   *
+   * Payload must be in the form of:
+   *
+   * {
+ *   id: "https://github.com/deconst/deconst-docs/issues/16" // Full url of content
+ *   body: { }
+ * }
+   *
+   */
+  server.put('/content', function (req, res, next) {
+    var dest = client.upload({
+      container: process.env.RACKSPACE_CONTAINER,
+      remote: encodeURIComponent(req.body.id)
+    });
+
+    dest.on('success', function () {
+      res.status(200);
+      res.send();
+      next();
+    });
+
+    // For now we're just going to JSON.stringify the body directly up to cloud files
+    // longer term we might do multi-plexing or async.parallel to different stores
+    dest.end(JSON.stringify(req.body.body));
+  });
+
+  /**
+   * @description Delete a piece of content by id
+   */
+  server.del('/content/:id', function (req, res, next) {
+    client.removeFile(process.env.RACKSPACE_CONTAINER, encodeURIComponent(req.params.id), function (err) {
+      if (err) {
+        res.status(err.statusCode);
+        res.send();
+        next();
+        return;
+      }
+
+      res.status(200);
+      res.send();
+      next();
+    });
+  });
+
+};

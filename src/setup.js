e7e95fc9c177a20e19f13f0d674565f779d2e516
Ash Wilson <smashwilson@gmail.com>
2015-04-02 09:25:46 -0400

    Create both containers on application launch.

--- src/setup.js
+++ src/setup.js
@@ -1,17 +1,29 @@
 // Actions to be performed on application launch.
 
+var async = require('async');
+
 /**
  * @description Create a function that asynchronously creates a Rackspace container if it
  *   doesn't already exist.
  */
-function make_container_creator(config, container_name) {
+function make_container_creator(client, container_name, cdn) {
   return function (callback) {
+    var handleCreation = callback;
+    if (cdn) {
+      handleCreation = function (err, container) {
+        container.enableCdn(callback);
+      };
+    }
+
     // Instead of checking if the container exists first, we try to create it, and
     // if it already exists, we get a no-op (202) and move on.
-    config.client.createContainer({ name: container_name }, callback);
+    client.createContainer({ name: container_name }, handleCreation);
   };
 }
 
 module.exports = function (config, callback) {
-  make_container_creator(config, config.content_container())(callback);
+  async.parallel([
+    make_container_creator(config.client, config.content_container(), false),
+    make_container_creator(config.client, config.asset_container(), true),
+  ], callback);
 };

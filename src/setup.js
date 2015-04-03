9b86450db42e293590062bb141969752ef6840e7
Ash Wilson <smashwilson@gmail.com>
2015-04-03 07:22:41 -0400

    Export container objects from setup.js.

--- src/setup.js
+++ src/setup.js
@@ -1,29 +1,44 @@
 // Actions to be performed on application launch.
 
-var async = require('async');
+var
+  async = require('async'),
+  pkgcloud = require('pkgcloud');
 
 /**
  * @description Create a function that asynchronously creates a Rackspace container if it
  *   doesn't already exist.
  */
-function make_container_creator(client, container_name, cdn) {
+function make_container_creator(client, container_name, logical_name, cdn) {
   return function (callback) {
-    var handleCreation = callback;
-    if (cdn) {
-      handleCreation = function (err, container) {
-        container.enableCdn(callback);
-      };
-    }
-
     // Instead of checking if the container exists first, we try to create it, and
     // if it already exists, we get a no-op (202) and move on.
-    client.createContainer({ name: container_name }, handleCreation);
+    client.createContainer({ name: container_name }, function (err, container) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      exports[logical_name] = container;
+
+      if (cdn) {
+        container.enableCdn(callback);
+      } else {
+        callback(null);
+      }
+    });
   };
 }
 
-module.exports = function (config, callback) {
+exports.setup = function (config, callback) {
+  var client = pkgcloud.providers.rackspace.storage.createClient({
+    username: config.rackspace_username(),
+    apiKey: config.rackspace_apikey(),
+    region: config.rackspace_region()
+  });
+  exports.client = client;
+
   async.parallel([
-    make_container_creator(config.client, config.content_container(), false),
-    make_container_creator(config.client, config.asset_container(), true),
+    make_container_creator(client, config.content_container(), "content_container", false),
+    make_container_creator(client, config.asset_container(), "asset_container", true),
   ], callback);
 };

d95f4829a3086a1f2196c9f37d7331d30b8df4bc
Ash Wilson <smashwilson@gmail.com>
2015-05-11 10:50:56 -0400

    Test case for indexing.

--- test/content.js
+++ test/content.js
@@ -47,6 +47,37 @@ describe("/content", function () {
         done);
     });
 
+    it("indexes content by category", function (done) {
+      var doc = {
+        title: "title goes here",
+        publish_date: "Tue, 05 Aug 2014 23:59:00 -0400",
+        body: "something",
+        tags: ["tag1", "tag2"],
+        categories: ["cat1", "cat2"]
+      };
+
+      request(server.create())
+        .put("/content/tagged")
+        .set("Authorization", authhelper.AUTH_USER)
+        .send(doc)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+
+          var contents = mocks.mock_db.collection("envelopes").find().toArray();
+
+          expect(contents).to.deep.include({
+            content_id: "tagged",
+            title: "title goes here",
+            publish_date: "Tue, 05 Aug 2014 23:59:00 -0400",
+            tags: ["tag1", "tag2"],
+            categories: ["cat1", "cat2"]
+          });
+
+          done();
+        });
+    });
+
   });
 
   describe("#retrieve", function () {

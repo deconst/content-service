da707eb4580f126d5ae55ab84e915ef2583f9230
Ash Wilson <smashwilson@gmail.com>
2015-08-03 13:00:00 -0400

    Toggle the logger on NODE_ENV.

--- src/logging.js
+++ src/logging.js
@@ -1,38 +1,52 @@
 var
-  winston = require('winston'),
-  config = require('./config');
+    winston = require('winston'),
+    config = require('./config');
 
 var logger = null;
 
 exports.getLogger = function () {
-  if (logger) return logger;
+    if (logger) return logger;
 
-  logger = new winston.Logger({
-    levels: {
-      trace: 0,
-      debug: 1,
-      verbose: 2,
-      info: 3,
-      warn: 4,
-      error: 5
-    },
-    colors: {
-      trace: 'white',
-      debug: 'grey',
-      verbose: 'cyan',
-      info: 'green',
-      warn: 'yellow',
-      error: 'red'
-    },
-    transports: [
-      new winston.transports.Console({
-        level: config.contentLogLevel().toLowerCase(),
-        prettyPrint: true,
-        colorize: config.contentLogColor(),
-        timestamp: true
-      })
-    ]
-  });
+    var activeTransports = [];
 
-  return logger;
+    if (process.env.NODE_ENV === 'production') {
+        activeTransports.push(new winston.transports.Console({
+            level: config.contentLogLevel().toLowerCase(),
+            prettyPrint: false,
+            colorize: config.contentLogColor(),
+            timestamp: true,
+            json: true,
+            stringify: true,
+            handleExceptions: true
+        }));
+    } else {
+        activeTransports.push(new winston.transports.Console({
+            level: config.contentLogLevel().toLowerCase(),
+            prettyPrint: true,
+            colorize: config.contentLogColor(),
+            timestamp: true,
+        }));
+    }
+
+    logger = new winston.Logger({
+        levels: {
+            trace: 0,
+            debug: 1,
+            verbose: 2,
+            info: 3,
+            warn: 4,
+            error: 5
+        },
+        colors: {
+            trace: 'white',
+            debug: 'grey',
+            verbose: 'cyan',
+            info: 'green',
+            warn: 'yellow',
+            error: 'red'
+        },
+        transports: activeTransports
+    });
+
+    return logger;
 };

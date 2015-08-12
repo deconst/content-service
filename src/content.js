6c0f4a7bb3549aa39cf834024bacdb7ad8a0877c
Ash Wilson <smashwilson@gmail.com>
2015-08-11 21:19:00 -0400

    Beautify content.js.

--- src/content.js
+++ src/content.js
@@ -1,55 +1,56 @@
 // Store, retrieve, and delete metadata envelopes.
 
-var
-    async = require('async'),
-    _ = require('lodash'),
-    restify = require('restify'),
-    config = require('./config'),
-    connection = require('./connection'),
-    log = require('./logging').getLogger();
-    assets = require('./assets');
+var async = require('async');
+var _ = require('lodash');
+var restify = require('restify');
+var config = require('./config');
+var connection = require('./connection');
+var log = require('./logging').getLogger();
+var assets = require('./assets');
 
 /**
  * @description Download the raw metadata envelope from Cloud Files.
  */
 function downloadContent(contentID, callback) {
-    var source = connection.client.download({
-        container: config.contentContainer(),
-        remote: encodeURIComponent(contentID)
+  var source = connection.client.download({
+    container: config.contentContainer(),
+    remote: encodeURIComponent(contentID)
+  });
+  var chunks = [];
+
+  source.on('error', function(err) {
+    callback(err);
+  });
+
+  source.on('data', function(chunk) {
+    chunks.push(chunk);
+  });
+
+  source.on('complete', function(resp) {
+    if (resp.statusCode === 404) {
+      return callback(new restify.NotFoundError("No content for ID [" + contentID + "]"));
+    }
+
+    var complete = Buffer.concat(chunks);
+
+    if (resp.statusCode > 400) {
+      log.warn({
+        action: 'contentretrieve',
+        contentID: contentID,
+        cloudFilesCode: resp.statusCode,
+        cloudFilesResponse: complete,
+        message: "Cloud Files error."
+      });
+
+      return callback(new restify.InternalServerError("Error communicating with an upstream service."));
+    }
+
+    var envelope = JSON.parse(complete);
+
+    callback(null, {
+      envelope: envelope
     });
-    var chunks = [];
-
-    source.on('error', function (err) {
-        callback(err);
-    });
-
-    source.on('data', function (chunk) {
-        chunks.push(chunk);
-    });
-
-    source.on('complete', function (resp) {
-        if (resp.statusCode === 404) {
-            return callback(new restify.NotFoundError("No content for ID [" + contentID + "]"));
-        }
-
-        var complete = Buffer.concat(chunks);
-
-        if (resp.statusCode > 400) {
-            log.warn({
-                action: 'contentretrieve',
-                contentID: contentID,
-                cloudFilesCode: resp.statusCode,
-                cloudFilesResponse: complete,
-                message: "Cloud Files error."
-            });
-
-            return callback(new restify.InternalServerError("Error communicating with an upstream service."));
-        }
-
-        var envelope = JSON.parse(complete);
-
-        callback(null, {envelope: envelope});
-    });
+  });
 }
 
 /**
@@ -57,10 +58,10 @@ function downloadContent(contentID, callback) {
  *   an outgoing metadata envelope.
  */
 function injectAssetVars(doc, callback) {
-    assets.enumerateNamed(function (err, assets) {
-        doc.assets = assets;
-        callback(null, doc);
-    });
+  assets.enumerateNamed(function(err, assets) {
+    doc.assets = assets;
+    callback(null, doc);
+  });
 }
 
 /**
@@ -68,239 +69,247 @@ function injectAssetVars(doc, callback) {
  *   results into the document.
  */
 function handleQueries(doc, callback) {
-    if (!doc.envelope.queries) {
-        return callback(null, doc);
-    }
+  if (!doc.envelope.queries) {
+    return callback(null, doc);
+  }
 
-    var queryNames = _.keys(doc.envelope.queries);
-    var queries = [];
-    for (var i = 0; i < queryNames.length; i++) {
-        queries.push(doc.envelope.queries[queryNames[i]]);
-    }
+  var queryNames = _.keys(doc.envelope.queries);
+  var queries = [];
+  for (var i = 0; i < queryNames.length; i++) {
+    queries.push(doc.envelope.queries[queryNames[i]]);
+  }
 
-    delete doc.envelope.queries;
+  delete doc.envelope.queries;
 
-    log.debug("Processing " + queries.length + " envelope queries.");
+  log.debug("Processing " + queries.length + " envelope queries.");
 
-    async.map(queries, handleQuery, function (err, results) {
-        if (err) return callback(err);
+  async.map(queries, handleQuery, function(err, results) {
+    if (err) return callback(err);
 
-        doc.results = {};
-        for (var i = 0; i < results.length; i++) {
-            for (var j = 0; j < results[i].length; j++) {
-                var each = results[i][j];
+    doc.results = {};
+    for (var i = 0; i < results.length; i++) {
+      for (var j = 0; j < results[i].length; j++) {
+        var each = results[i][j];
 
-                if (each.publish_date) {
-                    var d = new Date();
-                    d.setTime(each.publish_date);
-                    each.publish_date = d.toUTCString();
-                }
-            }
-
-            doc.results[queryNames[i]] = results[i];
+        if (each.publish_date) {
+          var d = new Date();
+          d.setTime(each.publish_date);
+          each.publish_date = d.toUTCString();
         }
+      }
 
-        callback(null, doc);
-    });
+      doc.results[queryNames[i]] = results[i];
+    }
+
+    callback(null, doc);
+  });
 }
 
 /**
  * @description Perform a single metadata envelope related-document query.
  */
 function handleQuery(query, callback) {
-    var order = query.$order || { publish_date: -1 };
-    var skip = query.$skip;
-    var limit = query.$limit;
+  var order = query.$order || {
+    publish_date: -1
+  };
+  var skip = query.$skip;
+  var limit = query.$limit;
 
-    if (query.$query) {
-        query = query.$query;
-        delete query.$query;
-    }
-    delete query.$skip;
-    delete query.$limit;
+  if (query.$query) {
+    query = query.$query;
+    delete query.$query;
+  }
+  delete query.$skip;
+  delete query.$limit;
 
-    var cursor = connection.db.collection("envelopes").find(query);
+  var cursor = connection.db.collection("envelopes").find(query);
 
-    cursor.sort(order);
-    if (skip) { cursor.skip(skip); }
-    if (limit) { cursor.limit(limit); }
+  cursor.sort(order);
+  if (skip) {
+    cursor.skip(skip);
+  }
+  if (limit) {
+    cursor.limit(limit);
+  }
 
-    cursor.toArray(callback);
+  cursor.toArray(callback);
 }
 
 /**
  * @description Store an incoming metadata envelope within Cloud Files.
  */
 function storeEnvelope(doc, callback) {
-    var dest = connection.client.upload({
-        container: config.contentContainer(),
-        remote: encodeURIComponent(doc.contentID)
-    });
+  var dest = connection.client.upload({
+    container: config.contentContainer(),
+    remote: encodeURIComponent(doc.contentID)
+  });
 
-    dest.end(JSON.stringify(doc.envelope), function (err) {
-        if (err) return callback(err);
+  dest.end(JSON.stringify(doc.envelope), function(err) {
+    if (err) return callback(err);
 
-        callback(null, doc);
-    });
+    callback(null, doc);
+  });
 }
 
 /**
  * @description Persist selected attributes from a metadata envelope in an indexed Mongo collection.
  */
 function indexEnvelope(doc, callback) {
-    var subdoc = _.pick(doc.envelope, ["title", "tags", "categories"]);
+  var subdoc = _.pick(doc.envelope, ["title", "tags", "categories"]);
 
-    subdoc.contentID = doc.contentID;
-    if (doc.envelope.publish_date) {
-        var parsed_date = Date.parse(doc.envelope.publish_date);
-        if (! Number.isNaN(parsed_date)) {
-            subdoc.publish_date = parsed_date;
-        }
+  subdoc.contentID = doc.contentID;
+  if (doc.envelope.publish_date) {
+    var parsed_date = Date.parse(doc.envelope.publish_date);
+    if (!Number.isNaN(parsed_date)) {
+      subdoc.publish_date = parsed_date;
     }
+  }
 
-    connection.db.collection("envelopes").findOneAndReplace(
-        { contentID: subdoc.contentID },
-        subdoc,
-        { upsert: true },
-        function (err) {
-            if (err) return callback(err);
-            callback(null, doc);
-        }
-    );
+  connection.db.collection("envelopes").findOneAndReplace({
+      contentID: subdoc.contentID
+    },
+    subdoc, {
+      upsert: true
+    },
+    function(err) {
+      if (err) return callback(err);
+      callback(null, doc);
+    }
+  );
 }
 
 /**
  * @description Retrieve content from the store by content ID.
  */
-exports.retrieve = function (req, res, next) {
-    log.debug({
+exports.retrieve = function(req, res, next) {
+  log.debug({
+    action: "contentretrieve",
+    contentID: req.params.id,
+    message: "Content ID request received."
+  });
+
+  var reqStart = Date.now();
+
+  async.waterfall([
+    async.apply(downloadContent, req.params.id),
+    injectAssetVars,
+    handleQueries
+  ], function(err, doc) {
+    if (err) {
+      log.error({
         action: "contentretrieve",
+        statusCode: err.statusCode || 500,
         contentID: req.params.id,
-        message: "Content ID request received."
+        error: err.message,
+        message: "Unable to retrieve content."
+      });
+
+      return next(err);
+    }
+
+    res.json(doc);
+
+    log.info({
+      action: "contentretrieve",
+      statusCode: 200,
+      contentID: req.params.id,
+      totalReqDuration: Date.now() - reqStart,
+      message: "Content request successful."
     });
 
-    var reqStart = Date.now();
-
-    async.waterfall([
-        async.apply(downloadContent, req.params.id),
-        injectAssetVars,
-        handleQueries
-    ], function (err, doc) {
-        if (err) {
-            log.error({
-                action: "contentretrieve",
-                statusCode: err.statusCode || 500,
-                contentID: req.params.id,
-                error: err.message,
-                message: "Unable to retrieve content."
-            });
-
-            return next(err);
-        }
-
-        res.json(doc);
-
-        log.info({
-            action: "contentretrieve",
-            statusCode: 200,
-            contentID: req.params.id,
-            totalReqDuration: Date.now() - reqStart,
-            message: "Content request successful."
-        });
-
-        next();
-    });
+    next();
+  });
 };
 
 /**
  * @description Store new content into the content service.
  */
-exports.store = function (req, res, next) {
-    log.debug({
+exports.store = function(req, res, next) {
+  log.debug({
+    action: "contentstore",
+    apikeyName: req.apikeyName,
+    contentID: req.params.id,
+    message: "Content storage request received."
+  });
+
+  var reqStart = Date.now();
+
+  var doc = {
+    contentID: req.params.id,
+    envelope: req.body
+  };
+
+  async.waterfall([
+    async.apply(storeEnvelope, doc),
+    indexEnvelope
+  ], function(err, doc) {
+    if (err) {
+      log.error({
         action: "contentstore",
+        statusCode: err.statusCode || 500,
         apikeyName: req.apikeyName,
         contentID: req.params.id,
-        message: "Content storage request received."
+        error: err.message,
+        totalReqDuration: Date.now() - reqStart,
+        message: "Unable to store content."
+      });
+
+      return next(err);
+    }
+
+    res.send(204);
+
+    log.info({
+      action: "contentstore",
+      statusCode: 204,
+      apikeyName: req.apikeyName,
+      contentID: req.params.id,
+      totalReqDuration: Date.now() - reqStart,
+      message: "Content storage successful."
     });
 
-    var reqStart = Date.now();
-
-    var doc = {
-        contentID: req.params.id,
-        envelope: req.body
-    };
-
-    async.waterfall([
-        async.apply(storeEnvelope, doc),
-        indexEnvelope
-    ], function (err, doc) {
-        if (err) {
-            log.error({
-                action: "contentstore",
-                statusCode: err.statusCode || 500,
-                apikeyName: req.apikeyName,
-                contentID: req.params.id,
-                error: err.message,
-                totalReqDuration: Date.now() - reqStart,
-                message: "Unable to store content."
-            });
-
-            return next(err);
-        }
-
-        res.send(204);
-
-        log.info({
-            action: "contentstore",
-            statusCode: 204,
-            apikeyName: req.apikeyName,
-            contentID: req.params.id,
-            totalReqDuration: Date.now() - reqStart,
-            message: "Content storage successful."
-        });
-
-        next();
-    });
+    next();
+  });
 };
 
 /**
  * @description Delete a piece of previously stored content by content ID.
  */
-exports.delete = function (req, res, next) {
-    log.debug({
+exports.delete = function(req, res, next) {
+  log.debug({
+    action: "contentdelete",
+    apikeyName: req.apikeyName,
+    contentID: req.params.id,
+    message: "Content deletion request received."
+  });
+
+  var reqStart = Date.now();
+
+  connection.client.removeFile(config.contentContainer(), encodeURIComponent(req.params.id), function(err) {
+    if (err) {
+      log.error({
         action: "contentdelete",
+        statusCode: err.statusCode || 500,
         apikeyName: req.apikeyName,
         contentID: req.params.id,
-        message: "Content deletion request received."
+        totalReqDuration: Date.now() - reqStart,
+        message: "Unable to delete content."
+      });
+
+      return next(err);
+    }
+
+    res.send(204);
+
+    log.info({
+      action: "contentdelete",
+      statusCode: 204,
+      apikeyName: req.apikeyName,
+      contentID: req.params.id,
+      totalReqDuration: Date.now() - reqStart,
+      message: "Content deletion successful."
     });
 
-    var reqStart = Date.now();
-
-    connection.client.removeFile(config.contentContainer(), encodeURIComponent(req.params.id), function (err) {
-        if (err) {
-            log.error({
-                action: "contentdelete",
-                statusCode: err.statusCode || 500,
-                apikeyName: req.apikeyName,
-                contentID: req.params.id,
-                totalReqDuration: Date.now() - reqStart,
-                message: "Unable to delete content."
-            });
-
-            return next(err);
-        }
-
-        res.send(204);
-
-        log.info({
-            action: "contentdelete",
-            statusCode: 204,
-            apikeyName: req.apikeyName,
-            contentID: req.params.id,
-            totalReqDuration: Date.now() - reqStart,
-            message: "Content deletion successful."
-        });
-
-        next();
-    });
+    next();
+  });
 };

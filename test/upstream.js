05711f95a468e7049752ff1adb17b522aa6a3136
Ash Wilson <smashwilson@gmail.com>
2016-03-02 13:36:35 -0500

    Test merged asset handling.

--- test/upstream.js
+++ test/upstream.js
@@ -11,6 +11,7 @@ const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 chai.use(dirtyChai);
 
+const async = require('async');
 const nock = require('nock');
 const request = require('supertest');
 const storage = require('../src/storage');
@@ -23,30 +24,101 @@ describe('upstream', () => {
     PROXY_UPSTREAM: 'https://upstream'
   }));
 
-  // Prepopulate local storage with content.
+  // Prepopulate local storage with content and two assets.
   beforeEach((done) => storage.storeContent('local', { body: 'local' }, done));
+  beforeEach((done) => {
+    // Note to self: this is extremely awkward.
+    let assets = [
+      {
+        key: 'only-local',
+        original: 'only-local.jpg',
+        filename: 'only-local-123123.jpg',
+        chunks: [],
+        type: 'image/jpg'
+      },
+      {
+        key: 'both',
+        original: 'both.jpg',
+        filename: 'both-456456.jpg',
+        chunks: [],
+        type: 'image/jpg'
+      }
+    ];
+
+    let storeEach = (asset, cb) => {
+      storage.storeAsset(asset, (err) => {
+        if (err) return cb(err);
+
+        storage.nameAsset(asset, cb);
+      });
+    };
+
+    async.each(assets, storeEach, done);
+  });
 
   it('returns local content the same', (done) => {
+    nock('https://upstream')
+      .get('/assets').reply(200, {});
+
     request(server.create())
       .get('/content/local')
       .set('Accept', 'application/json')
       .expect(200)
       .expect('Content-Type', 'application/json')
-      .expect({ assets: {}, envelope: { body: 'local' } }, done);
+      .expect({
+        assets: {
+          'only-local': '/__local_asset__/only-local-123123.jpg',
+          'both': '/__local_asset__/both-456456.jpg'
+        },
+        envelope: { body: 'local' }
+      }, done);
   });
 
-  it('returns assets merged from local and upstream');
+  it('returns assets merged from local and upstream', (done) => {
+    nock('https://upstream')
+      .get('/assets').reply(200, {
+        'only-upstream': 'https://assets.horse/up/only-upstream-123123.jpg',
+        'both': 'https://assets.horse/up/both-321321.jpg'
+      });
+
+    request(server.create())
+      .get('/content/local')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', 'application/json')
+      .expect({
+        assets: {
+          'only-upstream': 'https://assets.horse/up/only-upstream-123123.jpg',
+          'only-local': '/__local_asset__/only-local-123123.jpg',
+          'both': '/__local_asset__/both-456456.jpg'
+        },
+        envelope: { body: 'local' }
+      }, done);
+  });
 
   it('queries the upstream content service when content is not found locally', (done) => {
     nock('https://upstream')
-      .get('/content/remote').reply(200, { assets: {}, envelope: { body: 'remote' } });
+      .get('/content/remote').reply(200, {
+        assets: {
+          'only-upstream': 'https://assets.horse/up/only-upstream-123123.jpg',
+          'both': 'https://assets.horse/up/both-321321.jpg'
+        },
+        envelope: { body: 'remote' }
+      });
 
     request(server.create())
       .get('/content/remote')
       .set('Accept', 'application/json')
       .expect(200)
       .expect('Content-Type', 'application/json')
-      .expect({ assets: {}, envelope: { body: 'remote' } }, done);
+      .expect({
+        assets: {
+          'only-upstream': 'https://assets.horse/up/only-upstream-123123.jpg',
+          'only-local': '/__local_asset__/only-local-123123.jpg',
+          'both': '/__local_asset__/both-456456.jpg'
+        },
+        envelope: { body: 'remote' }
+      }, done);
   });
 
   it('propagates lookup failures from the upstream content service', (done) => {

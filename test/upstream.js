dd2cb508d3da650bd76c09398e2bd2821e14b0bd
Ash Wilson <smashwilson@gmail.com>
2016-03-02 10:07:50 -0500

    Test to ensure upstream doesn't break local retrieve.

--- test/upstream.js
+++ test/upstream.js
@@ -11,6 +11,9 @@ const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 chai.use(dirtyChai);
 
+const request = require('supertest');
+const storage = require('../src/storage');
+const server = require('../src/server');
 const resetHelper = require('./helpers/reset');
 
 describe('upstream', () => {
@@ -19,7 +22,18 @@ describe('upstream', () => {
     PROXY_UPSTREAM: 'upstream'
   }));
 
-  it('returns present content the same');
+  // Prepopulate local storage with content.
+  beforeEach((done) => storage.storeContent('local', { body: 'local' }, done));
+
+  it('returns local content the same', (done) => {
+    request(server.create())
+      .get('/content/local')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', 'application/json')
+      .expect({ assets: {}, envelope: { body: 'local' } }, done);
+  });
+
   it('queries the upstream content service when content is not found locally');
   it('propagates failures from the upstream content service');
 

428cc702f09450177fc6aeafd3d1e644a10b2913
Ash Wilson <smashwilson@gmail.com>
2016-04-07 14:12:02 -0400

    Unit test for /bulkasset

--- test/assets.js
+++ test/assets.js
@@ -1,3 +1,4 @@
+'use strict';
 /* global describe it beforeEach */
 
 /*
@@ -6,17 +7,21 @@
 
 require('./helpers/before');
 
-var fs = require('fs');
-var chai = require('chai');
-var dirtyChai = require('dirty-chai');
-
+const chai = require('chai');
+const dirtyChai = require('dirty-chai');
 chai.use(dirtyChai);
 
-var request = require('supertest');
-var resetHelper = require('./helpers/reset');
-var authHelper = require('./helpers/auth');
-var storage = require('../src/storage');
-var server = require('../src/server');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const async = require('async');
+const request = require('supertest');
+const tarfs = require('tar-fs');
+const getRawBody = require('raw-body');
+const resetHelper = require('./helpers/reset');
+const authHelper = require('./helpers/auth');
+const storage = require('../src/storage');
+const server = require('../src/server');
 
 describe('/assets', function () {
   // shasum -a 256 test/fixtures/asset-file.txt
@@ -91,5 +96,44 @@ describe('/assets', function () {
     request(server.create())
       .get('/assets/')
       .expect(200, done);
+  });
+});
+
+describe('/bulkasset', function () {
+  beforeEach(resetHelper);
+
+  const withFixtureTarball = function (fixtureName, andThen) {
+    return (cb) => {
+      let tarball = tarfs.pack(path.join(__dirname, 'fixtures', fixtureName)).pipe(zlib.createGzip());
+      getRawBody(tarball, (err, tarball) => {
+        if (err) return cb(err);
+
+        andThen(tarball, cb);
+      });
+    };
+  };
+
+  const expectStoredAsset = function (filename) {
+    return (cb) => storage.getAsset(filename, cb);
+  };
+
+  it('uploads all assets from a tarball', function (done) {
+    async.series([
+      withFixtureTarball('assets', (tarball, cb) => {
+        let r = request(server.create())
+          .post('/bulkasset')
+          .set('Authorization', authHelper.AUTH_USER)
+          .set('Content-Type', 'application/tar+gzip');
+
+        r.write(tarball);
+
+        r.expect(200)
+          .expect({ accepted: 2, failed: 0, deleted: 0 })
+          .end(cb);
+      }),
+      expectStoredAsset('style-260fccfc8f2a9f455bc7593b6aa9f97b9c59115450c52ea6d44b3833a1e9e158.css'),
+      expectStoredAsset('script-083f461dc2da33e85c40fccf85439168a9ec06d0ed45e2a107382d7d236d5b61.js'),
+      expectStoredAsset('dc-logo-0ec14d405ea0304b826fd8dc9de17638ed78cecec65b73d2b5847687da3c8e1f.png')
+    ], done);
   });
 });
---
+++ test/fixtures/assets/script.js
@@ -0,0 +1,3 @@
+'use strict';
+
+alert('HEY LISTEN');

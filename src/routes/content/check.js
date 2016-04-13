8298c96d799794700adeefbb5ffedc1b4f0df6df
Ash Wilson <smashwilson@gmail.com>
2016-04-13 14:39:22 -0400

    Recursive subqueries for ambiguous content IDs

--- src/routes/content/check.js
+++ src/routes/content/check.js
@@ -2,6 +2,7 @@
 
 // Test for the existence of metadata envelopes by fingerprint
 
+const _ = require('lodash');
 const restify = require('restify');
 const request = require('request');
 const urljoin = require('urljoin');
@@ -10,96 +11,128 @@ const storage = require('../../storage');
 const removeRevisionID = require('./retrieve').removeRevisionID;
 
 exports.handler = function (req, res, next) {
-  const contentIDMap = req.body;
+  const fingerprints = req.body;
 
-  storage.envelopesExist(contentIDMap, (err, existence) => {
+  storage.envelopesExist(fingerprints, (err, existence) => {
     if (err) {
       req.logger.handleError('Unable to query content with fingerprints', err);
       return next(new restify.errors.InternalServerError('Unable to query content fingerprints'));
     }
 
-    const results = {};
-    for (var contentID in existence) {
-      if (existence.hasOwnProperty(contentID)) {
-        results[contentID] = existence[contentID].present && existence[contentID].matches;
-      }
-    }
+    const localResults = _.mapValues(existence, (e) => e.present && e.matches);
 
-    const complete = () => {
+    const complete = (results) => {
       res.send(results);
       req.logger.reportSuccess('Content ID fingerprint query', {
-        envelopeCount: Object.keys(contentIDMap).length
+        envelopeCount: Object.keys(fingerprints).length
       });
       next();
     };
 
     if (config.proxyUpstream()) {
-      const url = urljoin(config.proxyUpstream(), 'checkcontent');
-
-      // Query upstream for all envelopes that are not present locally. Exclude any that were
-      // present but had different fingerprints. Remove the initial path segment when in staging
-      // mode, but track the mapping of modified content IDs to original ones.
-      const query = {};
-      const queryMapping = {};
-      for (let contentID in existence) {
-        if (existence.hasOwnProperty(contentID)) {
-          if (!existence[contentID].present) {
-            if (config.stagingMode()) {
-              const upstreamID = removeRevisionID(contentID);
-
-              // Remember which original contentID this upstreamID maps to. If multiple content IDs
-              // map to the same upstream ID, return an error.
-              if (upstreamID in queryMapping) {
-                req.logger.warn('Duplicate upstream content ID in upstream content fingerprint query', {
-                  contentID,
-                  upstreamID,
-                  statusCode: 400
-                });
-                return next(new restify.errors.BadRequestError('Multiple content IDs mapped to single upstream content ID'));
-              }
-              queryMapping[upstreamID] = contentID;
-
-              query[upstreamID] = contentIDMap[contentID];
-            } else {
-              query[contentID] = contentIDMap[contentID];
-            }
-          }
+      const query = { existence, fingerprints: {} };
+      _.forOwn(fingerprints, (fingerprint, contentID) => {
+        if (!existence[contentID].present) {
+          query.fingerprints[contentID] = fingerprint;
         }
-      }
-
-      req.logger.debug('Making an upstream content fingerprint query', {
-        url,
-        queryCount: Object.keys(query).length
       });
 
-      request({ url, body: query, json: true }, (err, response, body) => {
-        if (err) {
-          req.logger.reportError('Unable to query upstream with content fingerprints.', err);
-          return next(new restify.errors.BadGatewayError('Unable to query upstream for content fingerprints'));
-        }
-
-        if (response.statusCode !== 200) {
-          req.logger.reportError('Non-200 status from upstream content fingerprint check.');
-          return next(new restify.errors.BadGatewayError('Unable to query upstream for content fingerprints'));
-        }
-
-        // Merge in upstream results. When in staging mode, use queryMapping to report results
-        // in terms of the original query values.
-        for (let contentID in body) {
-          if (body.hasOwnProperty(contentID) && body[contentID]) {
-            if (config.stagingMode()) {
-              const originalID = queryMapping[contentID];
-              results[originalID] = true;
-            } else {
-              results[contentID] = true;
-            }
-          }
-        }
-
-        complete();
+      checkUpstream(req.logger, query, (err, upresults) => {
+        if (err) return next(err);
+        complete(_.assign(localResults, upresults));
       });
     } else {
-      complete();
+      complete(localResults);
+    }
+  });
+};
+
+/**
+ * @description Query an upstream content store for any envelopes that were not present locally.
+ * When staging mode is active as well, translate content IDs from the query by removing the
+ * initial path segment, then retranslate result IDs back to the original content IDs.
+ *
+ * If multiple content IDs from the query map to the same upstream content ID, a separate,
+ * recursive upstream query will be performed with the conflicting content IDs. Its results will
+ * merged in with this call's before the callback is invoked.
+ *
+ * Structure of the "query" parameter:
+ *
+ * {
+ *  existence: { <contentID>: { present: <Boolean>, matches: <Boolean> } },
+ *  fingerprints: { <contentID>: <fingerprint> }
+ * }
+ */
+const checkUpstream = function (logger, query, callback) {
+  const url = urljoin(config.proxyUpstream(), 'checkcontent');
+
+  const payload = {};
+  const mapping = {};
+  const subquery = {
+    existence: query.existence,
+    fingerprints: {}
+  };
+
+  for (let contentID in query.fingerprints) {
+    if (query.fingerprints.hasOwnProperty(contentID)) {
+      if (!query.existence[contentID].present) {
+        if (config.stagingMode()) {
+          const upstreamID = removeRevisionID(contentID);
+
+          // Remember the original contentID this upstreamID maps to. If multiple content IDs
+          // map to the same upstream ID, skip subsequent ones and instead begin constructing a
+          // subquery.
+          if (upstreamID in mapping) {
+            subquery.fingerprints[contentID] = query.fingerprints[contentID];
+          } else {
+            mapping[upstreamID] = contentID;
+            payload[upstreamID] = query.fingerprints[contentID];
+          }
+        } else {
+          // When not in staging mode, no contentID translation is necessary.
+          payload[contentID] = query.fingerprints[contentID];
+        }
+      }
+    }
+  }
+
+  logger.debug('Making an upstream content fingerprint query', {
+    url,
+    queryCount: Object.keys(payload).length
+  });
+
+  request({ url, body: payload, json: true }, (err, response, body) => {
+    if (err) {
+      logger.reportError('Unable to query upstream with content fingerprints.', err);
+      return callback(new restify.errors.BadGatewayError('Unable to query upstream for content fingerprints'));
+    }
+
+    if (response.statusCode !== 200) {
+      logger.reportError('Non-200 status from upstream content fingerprint check.', null, {
+        statusCode: response.statusCode
+      });
+
+      return callback(new restify.errors.BadGatewayError('Unable to query upstream for content fingerprints'));
+    }
+
+    let results = body;
+    if (config.stagingMode()) {
+      // When in staging mode, use the stored mapping to report results in terms of the original
+      // query contentIDs.
+      results = _.mapKeys(body, (v, upstreamID) => mapping[upstreamID]);
+    }
+
+    // Perform additional queries for ambiguous contentIDs if required.
+    if (Object.keys(subquery.fingerprints).length > 0) {
+      checkUpstream(logger, subquery, (err, subresults) => {
+        if (err) return callback(err);
+
+        // Merge subquery results into this query's.
+        callback(null, _.assign(results, subresults));
+      });
+    } else {
+      // No subquery necessary. Report directly.
+      callback(null, results);
     }
   });
 };

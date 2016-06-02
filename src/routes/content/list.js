152f175ebaec439641abd908a51ad823c2de661a
Ash Wilson <smashwilson@gmail.com>
2016-06-02 10:18:23 -0400

    Basic envelope enumeration.

--- src/routes/content/list.js
+++ src/routes/content/list.js
@@ -1,7 +1,28 @@
 'use strict';
 
+const storage = require('../../storage');
+
 exports.handler = function (req, res, next) {
   req.logger.debug('Content list requested.');
 
-  res.send(200, {});
+  const results = [];
+
+  const handleError = (message, err) => {
+    err.statusCode = err.status = 500;
+    req.logger.reportError(message, err);
+    return next(err);
+  };
+
+  storage.listEnvelopes(null, (err, each) => {
+    if (err) return handleError('Unable to retrieve envelope', err);
+
+    results.push({
+      contentID: each.contentID,
+      url: `/content/${encodeURIComponent(each.contentID)}`
+    });
+  }, (err) => {
+    if (err) return handleError('Unable to enumerate envelopes', err);
+
+    res.send(200, { total: 0, results });
+  });
 };
